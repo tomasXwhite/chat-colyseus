20dcb8ed0023c4adfc43c458bf41f0294dda3798
tomasxwhite <tomas8olivera@gmail.com>
2023-02-12 15:19:18 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ChatRoom></ChatRoom>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatRoom from './components/ChatRoom.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChatRoom,
   }
 }
 </script>
@@ -19,8 +19,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 30px; */
 }
 </style>
---
+++ src/components/ChatRoom.vue
@@ -0,0 +1,109 @@
+<template>
+    
+    <h1>Welcome to Real Time Chat App with Colyseus!</h1>
+    <div v-if="!joined" class="container">
+        <div class="nameContainer">
+            <input type="text" class="name" v-model="currentUser">
+            <button class="joinBtn" @click="join">Join</button>
+        </div>
+    </div>
+    <div v-if="joined" >
+        <!-- <div class="nameContainer">
+            <p>{{currentUser}} Joined</p>
+            <button class="signOutBtn" @click="signOut">SignOut</button>
+        </div> -->
+
+        <div class="messagesC">
+            <div v-for="message in messages" :key="message.id">
+            <b>{{ message.user }}</b>
+        : {{ message.text }}
+    </div>
+        </div>
+        <div class="chatContainer">
+            <textarea   
+            v-model="text"
+            class="textMessage"
+            v-on:keyup.enter="sendMessage">
+        </textarea>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChatRoom",
+    data() {
+        return {
+            joined: false,
+            currentUser: "",
+            text: "",
+            messages: [
+                {
+                    id: 1,
+                    text: "hi men",
+                    user: "manoj"
+                }
+            ],
+
+        }
+    },
+    methods: {
+        join() {
+            this.joined = true
+        },
+        signOut() {
+            this.joined = false
+            this.currentUser = ""
+        },
+        sendMessage() {
+console.log("asd");
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    position: fixed;
+    justify-content: center;
+    padding-top:50px;
+
+}
+.nameContainer{
+    display: flex;
+    flex-direction: column;
+}
+.name{
+    height: 30px;
+    font-size: 20px;
+    padding: 5px;
+    margin-bottom: 5px;
+    text-align: center;
+    font-weight: bold;
+
+}
+.joinBtn{
+    height: 30px;
+    font-size: 20px;
+}
+.signOutBtn{
+    height: 30px;
+    font-size: 20px;
+    color:red
+}
+.chatContainer{
+/* height: 100vh; */
+}
+.textMessage{
+    width: 97vw;
+    height: 80px;
+    position: absolute;
+    bottom:0px;
+    margin: 10px;
+    padding: 10px;
+
+}
+</style>
